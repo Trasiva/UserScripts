5adec659728e35543955654f7c7fb74337a15aa9
Trasiva <smartturkeycj@gmail.com>
2016-12-17 23:34:30 -0600

    Update Imgur Profile to Albums.user.js

--- Imgur Profile to Albums.user.js
+++ Imgur Profile to Albums.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Imgur: Check albums
 // @namespace    http://tampermonkey.net/
-// @version      0.13
+// @version      0.14
 // @description  View user albums from their profile
 // @author       Trasiva
 // @require      https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.6.15/browser-polyfill.min.js
@@ -14,7 +14,7 @@ var inline_src = (<><![CDATA[
 /* jshint ignore:end */
 /* jshint esnext: true */1
 
-const userName = window.location.href.replace(/.+\/user\/([a-zA-Z0-1]+)(\/favorites)?/, '$1');
+const userName = window.location.href.replace(/.+\/user\/([a-zA-Z0-1]+)(\/w+)?/, '$1');
 const submitButton = document.getElementById('pm-button');
 
 if (submitButton) {
