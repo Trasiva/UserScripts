b7d951408941f34db1a2d1e4aaa2d9f4eeb60f76
Trasiva <smartturkeycj@gmail.com>
2016-12-17 23:32:26 -0600

    Update Imgur Profile to Albums.user.js

--- Imgur Profile to Albums.user.js
+++ Imgur Profile to Albums.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Imgur: Check albums
 // @namespace    http://tampermonkey.net/
-// @version      0.12
+// @version      0.13
 // @description  View user albums from their profile
 // @author       Trasiva
 // @require      https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.6.15/browser-polyfill.min.js
@@ -14,28 +14,26 @@ var inline_src = (<><![CDATA[
 /* jshint ignore:end */
 /* jshint esnext: true */1
 
-const oldURL = 'http://imgur.com/user/corndogmaddy';
-const userName = window.location.href.replace(/.+\/user\//g, '');
-//const userName = oldURL.replace(/.+\/user\//g, '');
-
+const userName = window.location.href.replace(/.+\/user\/([a-zA-Z0-1]+)(\/favorites)?/, '$1');
 const submitButton = document.getElementById('pm-button');
 
 if (submitButton) {
     const newURL = `http://${userName}.imgur.com`;
+
     let profileLink = document.createElement('a');
     profileLink.setAttribute('href', newURL);
     profileLink.setAttribute('style', 'text-decoration: none;color:inherit');
-	
+    
     let profileButton = document.createElement('div');
     profileButton.setAttribute('class','button-css');
     profileButton.setAttribute('style', 'margin-top:5px');
     profileButton.innerText = `Check ${userName}'s Profile`;
 
     profileLink.appendChild(profileButton);
-  let parentDiv = submitButton.parentElement;
-  if (parentDiv) {
-  	parentDiv.appendChild(profileLink);
-  }
+    let parentDiv = submitButton.parentElement;
+    if (parentDiv) {
+        parentDiv.appendChild(profileLink);
+    }
 }
 
 /* jshint ignore:start */
