1fd75fc9a5e404f3cce27dd7f4dcf775ee9b8674
Trasiva <smartturkeycj@gmail.com>
2018-12-09 01:29:56 -0600

    Update Imgur Post to Albums.user.js

--- Imgur Post to Albums.user.js
+++ Imgur Post to Albums.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Imgur: Check albums from post
 // @namespace    http://tampermonkey.net/
-// @version      0.17
+// @version      0.18
 // @description  View user albums from their post
 // @author       Trasiva
 // @match        https://imgur.com/gallery/*
@@ -10,44 +10,49 @@
 // ==/UserScript==
 
 window.addEventListener('load', function() {
-    console.log('event hit');
-    let postHeader = document.getElementsByClassName('post-header');
+    let divNav = document.querySelector('div[class="next-prev"]');
+    
+    if (divNav != null) {
+        divNav.AddEventListener('click', function() {
+            let postHeader = document.getElementsByClassName('post-header');
 
-    if (postHeader.length > 0) {
-        postHeader = postHeader[0];
+            if (postHeader.length > 0) {
+                postHeader = postHeader[0];
 
-        let postAccount = postHeader.getElementsByClassName('post-account');
-        if (postAccount.length > 0) {
-            postAccount = postAccount[0];
-            const userName = postAccount.innerText;
-            const newURL = `http://${userName}.imgur.com`;
+                let postAccount = postHeader.getElementsByClassName('post-account');
+                if (postAccount.length > 0) {
+                    postAccount = postAccount[0];
+                    const userName = postAccount.innerText;
+                    const newURL = `http://${userName}.imgur.com`;
 
-            let profileLink = document.createElement('a');
-            profileLink.setAttribute('href', newURL);
-            profileLink.setAttribute('class', 'post-account');
-            profileLink.setAttribute('style', 'margin-left:10px');
-            profileLink.setAttribute('target', '_blank');
-            profileLink.innerText = `Profile`;      
+                    let profileLink = document.createElement('a');
+                    profileLink.setAttribute('href', newURL);
+                    profileLink.setAttribute('class', 'post-account');
+                    profileLink.setAttribute('style', 'margin-left:10px');
+                    profileLink.setAttribute('target', '_blank');
+                    profileLink.innerText = `Profile`;      
 
-            const urlType = window.location.href.replace(/.+.com\/(.+)\/.+/g, '$1');
-            let downloadLink = document.createElement('a');
+                    const urlType = window.location.href.replace(/.+.com\/(.+)\/.+/g, '$1');
+                    let downloadLink = document.createElement('a');
 
-            if (urlType === 'a') {
-                const downloadURL = `${window.location.href}/href`;
-                downloadLink.setAttribute('href', downloadURL);
-                downloadLink.setAttribute('class', 'post-account');
-                downloadLink.setAttribute('style', 'margin-left:10px');
-                downloadLink.setAttribute('target', '_blank');
-                downloadLink.innerText = `Download`; 
-            }
+                    if (urlType === 'a') {
+                        const downloadURL = `${window.location.href}/href`;
+                        downloadLink.setAttribute('href', downloadURL);
+                        downloadLink.setAttribute('class', 'post-account');
+                        downloadLink.setAttribute('style', 'margin-left:10px');
+                        downloadLink.setAttribute('target', '_blank');
+                        downloadLink.innerText = `Download`; 
+                    }
 
-            let divContainer = postHeader.getElementsByClassName('post-title-meta');
-            if (divContainer.length > 0) {
-                divContainer[0].appendChild(profileLink);
-                if (urlType === 'a') {
-                    divContainer[0].appendChild(downloadLink); 
+                    let divContainer = postHeader.getElementsByClassName('post-title-meta');
+                    if (divContainer.length > 0) {
+                        divContainer[0].appendChild(profileLink);
+                        if (urlType === 'a') {
+                            divContainer[0].appendChild(downloadLink); 
+                        }
+                    }
                 }
             }
-        }
+        }, false)
     }
 }, false);
