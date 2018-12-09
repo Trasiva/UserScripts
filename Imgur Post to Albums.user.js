8da852ab8d7d53596504ac3d62b552634d704b76
Trasiva <smartturkeycj@gmail.com>
2018-12-08 19:09:03 -0600

    Update Imgur Post to Albums.user.js
    Fixed based on Imgur and Codemonkey changes

--- Imgur Post to Albums.user.js
+++ Imgur Post to Albums.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Imgur: Check albums from post
 // @namespace    http://tampermonkey.net/
-// @version      0.15
+// @version      0.16
 // @description  View user albums from their post
 // @author       Trasiva
 // @match        https://imgur.com/gallery/*
@@ -9,53 +9,44 @@
 // @match        https://imgur.com/*/favorites/*
 // ==/UserScript==
 
-/* jshint ignore:start */
-var inline_src = (<><![CDATA[
-/* jshint ignore:end */
-/* jshint esnext: true */
+window.addEventListener('load', function() {
+    let postHeader = document.getElementsByClassName('post-header');
 
-let postHeader = document.getElementsByClassName('post-header');
+    if (postHeader.length > 0) {
+        postHeader = postHeader[0];
 
-if (postHeader.length > 0) {
-    postHeader = postHeader[0];
-    
-    let postAccount = postHeader.getElementsByClassName('post-account');
-    if (postAccount.length > 0) {
-        postAccount = postAccount[0];
-        const userName = postAccount.innerText;
-        const newURL = `http://${userName}.imgur.com`;
-        
-        let profileLink = document.createElement('a');
-        profileLink.setAttribute('href', newURL);
-        profileLink.setAttribute('class', 'post-account');
-        profileLink.setAttribute('style', 'margin-left:10px');
-        profileLink.setAttribute('target', '_blank');
-        profileLink.innerText = `Profile`;      
+        let postAccount = postHeader.getElementsByClassName('post-account');
+        if (postAccount.length > 0) {
+            postAccount = postAccount[0];
+            const userName = postAccount.innerText;
+            const newURL = `http://${userName}.imgur.com`;
 
-        const urlType = window.location.href.replace(/.+.com\/(.+)\/.+/g, '$1');
-        let downloadLink = document.createElement('a');
+            let profileLink = document.createElement('a');
+            profileLink.setAttribute('href', newURL);
+            profileLink.setAttribute('class', 'post-account');
+            profileLink.setAttribute('style', 'margin-left:10px');
+            profileLink.setAttribute('target', '_blank');
+            profileLink.innerText = `Profile`;      
 
-        if (urlType === 'a') {
-            const downloadURL = `${window.location.href}/href`;
-            downloadLink.setAttribute('href', downloadURL);
-            downloadLink.setAttribute('class', 'post-account');
-            downloadLink.setAttribute('style', 'margin-left:10px');
-            downloadLink.setAttribute('target', '_blank');
-            downloadLink.innerText = `Download`; 
-        }
+            const urlType = window.location.href.replace(/.+.com\/(.+)\/.+/g, '$1');
+            let downloadLink = document.createElement('a');
 
-        let divContainer = postHeader.getElementsByClassName('post-title-meta');
-        if (divContainer.length > 0) {
-            divContainer[0].appendChild(profileLink);
             if (urlType === 'a') {
-                divContainer[0].appendChild(downloadLink); 
+                const downloadURL = `${window.location.href}/href`;
+                downloadLink.setAttribute('href', downloadURL);
+                downloadLink.setAttribute('class', 'post-account');
+                downloadLink.setAttribute('style', 'margin-left:10px');
+                downloadLink.setAttribute('target', '_blank');
+                downloadLink.innerText = `Download`; 
+            }
+
+            let divContainer = postHeader.getElementsByClassName('post-title-meta');
+            if (divContainer.length > 0) {
+                divContainer[0].appendChild(profileLink);
+                if (urlType === 'a') {
+                    divContainer[0].appendChild(downloadLink); 
+                }
             }
         }
     }
-}
-
-/* jshint ignore:start */
-]]></>).toString();
-var c = babel.transform(inline_src);
-eval(c.code);
-/* jshint ignore:end */
+}, false);
