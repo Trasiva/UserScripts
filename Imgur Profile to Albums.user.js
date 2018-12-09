0a347aaa5ab2310a7473096a0c3c3353ce86c2e3
Trasiva <smartturkeycj@gmail.com>
2018-12-08 19:01:17 -0600

    Update Imgur Profile to Albums.user.js

--- Imgur Profile to Albums.user.js
+++ Imgur Profile to Albums.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Imgur: Check albums
 // @namespace    http://tampermonkey.net/
-// @version      0.21
+// @version      0.22
 // @description  View user albums from their profile
 // @author       Trasiva
 // @match        https://imgur.com/user/*
