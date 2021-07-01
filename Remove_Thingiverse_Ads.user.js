09c56befddb2b0a80105f0a9d12c96cb72e56cb2
Trasiva <smartturkeycj@gmail.com>
2021-06-30 23:13:14 -0500

    Create Remove_Thingiverse_Ads.user.js

---
+++ Remove_Thingiverse_Ads.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Remove Thingiverse Ads
+// @namespace    http://tampermonkey.net/
+// @version      0.02
+// @description  Remove the stupid reddit adds that bypass pihole
+// @author       Trasiva
+// @match        https://www.thingiverse.com/*
+// ==/UserScript==
+
+window.addEventListener('load', function() {
+	let nodes = document.querySelectorAll('div[class*="AdCard"]');
+  console.log(`Removing ${nodes.length} nodes.`)
+  for (let iter = nodes.length - 1; iter >= 0; iter--) {
+      nodes[iter].remove();
+  }
+}, false);
