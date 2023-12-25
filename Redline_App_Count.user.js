533634b29a7f71d7495335fb56512be46fc86f11
Trasiva <smartturkeycj@gmail.com>
2023-12-24 21:08:24 -0600

    Update Redline_App_Count.user.js

--- Redline_App_Count.user.js
+++ Redline_App_Count.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Redline Whitelist Appysystems
 // @namespace    http://tampermonkey.net/
-// @version      0.01
+// @version      0.02
 // @description  Update pending and processing app section with count
 // @author       Trasiva
 // @match        https://appsystems.co.uk/centre/manage/redlinerp
@@ -11,14 +11,14 @@ window.addEventListener('load', function() {
     const container = document.querySelector('#processing_grid')
 
     if (container != null) {
-        GetAppCountByType('apps-grid', 'search_entry_1')
-        GetAppCountByType('processing_grid', 'search_entry_2')
+        GetAppCountByType('#apps-grid', '#search_entry_1')
+        GetAppCountByType('#processing_grid', '#search_entry_2')
     }
 }, false);
 
 function GetAppCountByType(gridName, childElementName) {
-    const apps = document.querySelectorAll(`#${gridName} > .app-container`)
-    let textLabel = document.querySelector(`#${childElementName}`).parentElement()
+    const apps = document.querySelectorAll(`${gridName} > .app-container`)
+    let textLabel = document.querySelector(childElementName).parentElement()
 
     textLabel.innerText = `${textLabel.innerText} (${apps.length})`
 }
