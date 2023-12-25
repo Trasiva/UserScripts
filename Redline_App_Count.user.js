e5295ea7faa0ffc9626fa18479c338c8fb76bb42
Trasiva <smartturkeycj@gmail.com>
2023-12-24 22:18:11 -0600

    Update Redline_App_Count.user.js
    Update to 0.09

--- Redline_App_Count.user.js
+++ Redline_App_Count.user.js
@@ -1,17 +1,18 @@
 // ==UserScript==
 // @name         Redline Whitelist Appysystems
 // @namespace    http://tampermonkey.net/
-// @version      0.06
+// @version      0.09
 // @description  Update pending and processing app section with count
 // @author       Trasiva
 // @match        https://appsystems.co.uk/centre/manage/redlinerp
 // ==/UserScript==
 
-window.addEventListener('load', function() {
+window.addEventListener('load',async function() {
     const container = document.querySelector('#processing_grid')
 
     if (container != null) {
-        GetAppCountByType('#apps-grid', '#search_entry_1')
+        await(sleep(2000))
+        GetAppCountByType('#pending_grid', '#search_entry_1')
         GetAppCountByType('#processing_grid', '#search_entry_2')
     }
 }, false);
@@ -19,15 +20,18 @@ window.addEventListener('load', function() {
 async function GetAppCountByType(gridName, childElementName) {
     const maxCount = 5
     let tryCount = 1
-    while (tryCount < maxCount) {
-        console.log(`Polling ${gridName} ${tryCount} of ${maxCount}`)
+    while (tryCount <= maxCount) {
         const apps = document.querySelectorAll(`${gridName} > .app-container`)
-        const appCount = apps.length
-        let textLabel = document.querySelector(childElementName).parentElement
+        const searchBar = document.querySelector(childElementName)
+        let appCount = apps.length
 
-        textLabel.innerText = `${textLabel.innerText} (${apps.length})`
-
-        if (appCount == 0) {
+        if (appCount > 0 || tryCount == maxCount) {
+            let textLabel = searchBar.parentElement
+            textLabel.innerHTML = textLabel.innerHTML.replace("Applications", `Applications (${apps.length})`)
+            console.log(`Found ${appCount} apps for ${gridName}`)
+            break
+        }
+        else {
             tryCount++
             await(sleep(2000))
         }
