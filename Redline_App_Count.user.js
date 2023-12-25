8131cf47e2c9017edce11de8a36894993012bc81
Trasiva <smartturkeycj@gmail.com>
2023-12-24 21:22:29 -0600

    Update Redline_App_Count.user.js
    Update to 0.06

--- Redline_App_Count.user.js
+++ Redline_App_Count.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Redline Whitelist Appysystems
 // @namespace    http://tampermonkey.net/
-// @version      0.05
+// @version      0.06
 // @description  Update pending and processing app section with count
 // @author       Trasiva
 // @match        https://appsystems.co.uk/centre/manage/redlinerp
@@ -20,12 +20,17 @@ async function GetAppCountByType(gridName, childElementName) {
     const maxCount = 5
     let tryCount = 1
     while (tryCount < maxCount) {
+        console.log(`Polling ${gridName} ${tryCount} of ${maxCount}`)
         const apps = document.querySelectorAll(`${gridName} > .app-container`)
         const appCount = apps.length
         let textLabel = document.querySelector(childElementName).parentElement
 
         textLabel.innerText = `${textLabel.innerText} (${apps.length})`
-        await(sleep(2000))
+
+        if (appCount == 0) {
+            tryCount++
+            await(sleep(2000))
+        }
     }
 }
 
